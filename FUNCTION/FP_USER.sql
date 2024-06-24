bd53d34afaa10d11e81f9dc88f91807d8dcf6731
Jorge Mendez <56201939+mendezjorge@users.noreply.github.com>
2024-06-24 07:52:50 -0400

    user=jorge HOST=WORKGROUP\ASUS DDL change detected: CREATE on FP_USER 24/06/2024 11:52:49

---
+++ FUNCTION/FP_USER.sql
@@ -0,0 +1,10 @@
+
+  CREATE OR REPLACE EDITIONABLE FUNCTION "SKN"."FP_USER" return varchar2 is
+  v_return varchar2(2000);
+begin
+  v_return := nvl(v('APP_USER'), user);
+  return v_return;
+exception
+  when others then
+    return user;
+end fp_user;
